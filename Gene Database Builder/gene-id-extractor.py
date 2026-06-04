f5915c1c4eb01fb424e66be52853c7bc1da1a7e1
Helyphion <helyphion@gmail.com>
2026-06-04 15:13:03 -0300

    Add gene names into JSON

--- Gene Database Builder/gene-id-extractor.py
+++ Gene Database Builder/gene-id-extractor.py
@@ -23,32 +23,41 @@ outputDict["tertiary"]["rare"] = {}
 with open("data/all-genes-plaintext.txt", "r") as file:
     allGenesPlaintextFile = file.read().splitlines()
 
-geneNames = []
-
-for x in allGenesPlaintextFile:
-    # filters out all the stuff that is not gene names
-    if not x.isupper() and x != "":
-        geneNames.append( x.lower().split(", ") )
-
-
-for x in geneNames[0]:
-    outputDict["primary"]["common"][x] = {}
-    print(x)
-outputDict["primary"]["uncommon"] = geneNames[1]
-
-print(outputDict)
-
-"""
 with open("data/modern-prims-html.txt", "r") as file:
     modernPrims = file.read().splitlines()
 
 for x in modernPrims:
     geneID = int(x.split('"')[1])
     # the [:-8] trims "</option" bit from extracted name
-    geneName = x.split('>')[1][:-8]
+    geneName = x.lower().split('>')[1][:-8]
     outputDict[geneName] = {"modern": geneID}
 
-"""
+geneNames = []
+
+for x in allGenesPlaintextFile:
+    # filters out the stuff from the plaintext file that is not gene names
+    if not x.isupper() and x != "":
+        geneNames.append( x.lower().split(", ") )
+# list contain 12 lists of gene names, first list is primary-common, second is primary-uncommon, and so on
+
+
+# adds gene names to the JSON (once again janky, but good enough for now)
+geneSlots = ["primary", "secondary", "tertiary"]
+rarities = ["common", "uncommon", "limited", "rare"]
+slot = 0
+rar = 0
+for x in geneNames:
+    for y in x:
+        outputDict[geneSlots[slot]][rarities[rar]][y] = {}
+        print(y)
+    rar += 1
+    # cycles thru the 4 rarities, once at the end of that, resets to first rarity, and switches gene slot
+    if rar == 4:
+        rar = 0
+        slot += 1
+# this does rely on there being exactly 12 lines of gene data in the plaintext file to work correctly, just keep that in mind
+
+
 
 test = json.dumps(outputDict, indent=4)
 
