e2f1fa1a1c38d17778388353561eeb19dd555fa5
Helyphion <helyphion@gmail.com>
2026-06-08 17:05:54 -0300

    Mostly experimentation if I'm honest

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-const primRarity = document.getElementById("prim-rarity")
+function parseRarity() {
+    const test = document.getElementByClass("body")
+    document.body.style.backgroundColor = "black"
+}
+
+const commonPrim = document.getElementById("common-prim");
+commonPrim.addEventListener("change", )
 
 // remember that basic is id 0 lol
