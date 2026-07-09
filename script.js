b4acf4d4147a0f416f3a115da348b22a46a08307
Helyphion <helyphion@gmail.com>
2026-07-09 16:49:15 -0300

    Attempts at loading in the gene rarities JSON and parsing it

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-function parseRarity() {
-    console.log("TEST")
+function parseRarity(givenForm) {
 
-    const test = new FormData(primRarityForm);
-    console.log(test.getAll("prim-rarity"));
+    const formContents = new FormData(givenForm);
+    console.log(formContents.getAll("rarity"));
+    // howw.. do I get this data out of here 
     // now if only I had made the json layout make more sense. lmao
 
 }
 
+async function getGeneIdsJson() {
+    const response = await fetch("gene-rarities.json");
+    const geneIdsJson = await response.json();
+    console.log(geneIdsJson);
+}
 
-const primRarityForm = document.getElementById("prim-rarity");
 
-primRarityForm.addEventListener("change", parseRarity)
+async function main() {
+
+    const geneIdsJson = await getGeneIdsJson();
+
+    const primRarityForm = document.getElementById("prim-rarity");
+    primRarityForm.addEventListener("change", () => parseRarity(primRarityForm))
+    // arrow functions are necessary for parameters to work
+
+
+    
+    // this is just for debugging bc my setup is.. janky rn:
+    document.body.style.backgroundColor = "black";
+
+}
+
+main();
 
 
 // example link:
@@ -18,6 +37,3 @@ primRarityForm.addEventListener("change", parseRarity)
 // "and" combinator for several selections seems to be %2C
 
 // remember that basic is id 0 lol
-
-// this is just for debugging bc my setup is.. janky rn:
-document.body.style.backgroundColor = "black";
