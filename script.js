14ecf27d2684a52d8fd026cacd11e12fca8e657a
Helyphion <helyphion@gmail.com>
2026-07-09 15:59:37 -0300

    Restructure HTML and apply rudimentary CSS

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ const primRarityForm = document.getElementById("prim-rarity");
 primRarityForm.addEventListener("change", parseRarity)
 
 
-
+// example link:
+// https://www1.flightrising.com/auction-house/buy/realm/dragons?d_gender=0&d_bodygene=110%2C10%2C213&d_winggene=0%2C213&d_tertgene=22&collapse=1
+// "and" combinator for several selections seems to be %2C
 
 // remember that basic is id 0 lol
 
