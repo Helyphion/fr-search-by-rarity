725bda9bbfcbd130b753cf0cf8772fa4de3e6e6c
Helyphion <helyphion@gmail.com>
2026-07-09 13:57:01 -0300

    Import Bootstrap, and.. other stuff. idk I forgot to commit before I left last time

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 function parseRarity() {
-    const test = document.getElementByClass("body")
-    document.body.style.backgroundColor = "black"
+    const primRarityCheckboxes = primRarity.elements;
+    console.log(primRarityCheckboxes);
+
+    /*
+    for (const input of primRarityCheckboxes) {
+        if (input.nodeName === "INPUT" && input.type === "checkbox") {
+          console.log(input)
+        }
+      }
+    */
+
+    // ughh I don't know man. I give up. (for now)
+    const formData = new FormData(primRarity);
+
+    // 2. Get all values associated with the checkbox name attribute
+    const selectedValues = formData.getAll("prim-rarity");
+    
+    console.log(selectedValues);
 }
 
-const commonPrim = document.getElementById("common-prim");
-commonPrim.addEventListener("change", )
+
+const primRarity = document.getElementById("prim-rarity");
+primRarity.addEventListener("change", parseRarity())
+
+
+
+
 
 // remember that basic is id 0 lol
+
+// this is just for debugging bc my setup is.. janky rn:
+// document.body.style.backgroundColor = "black";
